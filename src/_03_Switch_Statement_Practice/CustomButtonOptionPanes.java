2d27ffe8b1f1c1f4ea8e061e597e14336aef20b0
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-07-09 11:20:43 -0700

    Switch Statements, Zodiac

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,28 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
-		
+		switch (choice) {
+		case "Sunday":
+			System.out.println("It is Sunday");
+			break;
+		case "Monday":
+			System.out.println("It is Monday");
+			break;
+		case "Tuesday":
+			System.out.println("Taco Tuesday");
+			break;
+		case "Wednesday":
+			System.out.println("Hump Daaayy");
+			break;
+		case "Thursday":
+			System.out.println("Day after hump day");
+			break;
+		case "Friday":
+			System.out.println("Whoop Whoop");
+			break;
+		case "Saturday":
+			System.out.println("Whoopty Shmoopty");
+			break;
+		}
 	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -2,13 +2,55 @@ package _05_Enum_Stuff;
 
 import javax.swing.JOptionPane;
 
-public class _00_Horoscope {
+public class _00_Horoscope{
 	// 1. Create an enum in a separate file called Zodiac that contains a category for
 	//    all 12 zodiac signs.
-	
+	Zodiac zodiac;
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	
+	void display() {
+		switch (zodiac) {
+		case ARIES:
+			
+			break;
+		case AQUARIUS:
+			
+			break;
+		case CANCER:
+			
+			break;
+		case CAPRICORN:
+			
+			break;
+		case GEMINI:
+			
+			break;
+		case LEO:
+			
+			break;
+		case LIBRA:
+			
+			break;
+		case PISCES:
+			
+			break;
+		case SAGITTARIUS:
+			
+			break;
+		case SCORPIO:
+			
+			break;
+		case TAURUS:
+			
+			break;
+		case VIRGO:
+			
+			break;			
+		}
+	}
 	// 3. Make a main method to test your method
-	
+	public static void main(String[] args) {
+		_00_Horoscope h = new _00_Horoscope();
+		h.display();
+	}
 }
